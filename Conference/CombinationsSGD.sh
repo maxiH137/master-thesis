9c97d12a20f4d4ad5a38b04182b973557e1efb69
Maximilian <maximilian2.hopp@student.uni-siegen.de>
2025-04-11 19:54:57 +0200

    Combinations for SGD setting and .sh execution commands

---
+++ Conference/CombinationsSGD.sh
@@ -0,0 +1,96 @@
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling shuffle --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling unbalanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling balanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling sequential --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling shuffle --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling unbalanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling balanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling sequential --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling shuffle --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling unbalanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling balanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling sequential --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling shuffle --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling unbalanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling balanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling sequential --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling shuffle --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling unbalanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling balanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling sequential --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling shuffle --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling unbalanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling balanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling sequential --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling shuffle --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling unbalanced --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling balanced --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling sequential --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling shuffle --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling unbalanced --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling balanced --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml --sampling sequential --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling shuffle --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling unbalanced --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling balanced --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling sequential --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling shuffle --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling unbalanced --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling balanced --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling sequential --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling shuffle --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling unbalanced --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling balanced --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_deep.yaml --sampling sequential --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling shuffle --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling unbalanced --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling balanced --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_deep.yaml--sampling sequential --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling shuffle --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling unbalanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling balanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling sequential --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling shuffle --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling unbalanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling balanced --batch_size 1 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling sequential --batch_size 1 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling shuffle --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling unbalanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling balanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling sequential --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling shuffle --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling unbalanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling balanced --batch_size 10 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml--sampling sequential --batch_size 10 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling shuffle --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling unbalanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling balanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling sequential --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling shuffle --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling unbalanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling balanced --batch_size 100 --trained
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling sequential --batch_size 100 --trained
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling shuffle --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling unbalanced --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling balanced --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling sequential --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling shuffle --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling unbalanced --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling balanced --batch_size 1 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling sequential --batch_size 1 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling shuffle --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling unbalanced --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling balanced --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling sequential --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling shuffle --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling unbalanced --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling balanced --batch_size 10 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling sequential --batch_size 10 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling shuffle --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling unbalanced --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling balanced --batch_size 100 
+Leakage.py --./configs/leakage/wear_loso_tiny.yaml --sampling sequential --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling shuffle --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling unbalanced --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling balanced --batch_size 100 
+Leakage.py --./configs/leakage/wetlab_loso_tiny.yaml --sampling sequential --batch_size 100 
