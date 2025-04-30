f7d06d9d2aa3f3abb0cd668726c4a5b88565f703
maxiH <62959323+maxiH137@users.noreply.github.com>
2025-04-30 11:32:02 +0200

    Sh commands for Local Updates case

---
+++ Conference/CombinationsLocalUpdates.sh
@@ -0,0 +1,8 @@
+LeakageAVG_local.py --./configs/leakage/wear_loso_deep.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_deep.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wear_loso_deep.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_deep.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wear_loso_tiny.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_tiny.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wear_loso_tiny.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_tiny.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5
---
+++ Conference/CombinationsLocalUpdatesDP.sh
@@ -0,0 +1,8 @@
+LeakageAVG_local.py --./configs/leakage/wear_loso_deep.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_deep.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wear_loso_deep.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_deep.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wear_loso_tiny.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_tiny.yaml --trained --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wear_loso_tiny.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
+LeakageAVG_local.py --./configs/leakage/wetlab_loso_tiny.yaml --num_data_points 500 --num_data_per_local_update_step 100 --num_local_updates 5 --clipping 0.5 --noise 0.1
